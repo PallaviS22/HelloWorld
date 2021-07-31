52bbdcfd7401ea56173582a8db2235357be9f666
PallaviS22 <rpallaviteli@gmail.com>
2021-08-01 00:49:40 +0530

    Added textfile

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello"); 
         System.out.println("I have added this new line and will commt to check difference"); 
+        System.out.println("Automatically detected by Jenkins.......Wowowoow");
     }
 }
